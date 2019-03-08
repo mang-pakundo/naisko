db51140c8c030f72c968ff385ec5df22578b7e19
mang-pakundo <mang-pakundo@users.noreply.github.com>
2019-03-08 12:57:30 -0600

    Added play fix Added icon Cleanup string and settings

--- default.py
+++ default.py
@@ -11,6 +11,7 @@ import xbmcgui
 import xbmcplugin
 import xbmcaddon
 
+from random import randint
 from pprint import pprint
 
 user_agent = 'okhttp/3.11.0'
@@ -25,6 +26,7 @@ mode_genre = 2
 mode_show = 3
 mode_episode = 4
 mode_play = 5
+mode_play_live = 6
 
 def build_url(path, base_url = base_url, params = {}):
     url = '{base_url}{path}'.format(base_url = base_url, path = path)
@@ -34,6 +36,8 @@ def build_url(path, base_url = base_url, params = {}):
     
 def http_request(url, params = {}, headers = {}):
     req = urllib2.Request(url)
+    if not is_x_forwarded_for_ip_valid():
+        auto_generate_ip()
     req.add_header('X-Forwarded-For', this_addon.getSetting('xForwardedForIp'))
     req.add_header('User-Agent', user_agent)
     for k, v in headers.iteritems():
@@ -61,17 +65,19 @@ def add_dir(name, id, mode, is_folder = True, **kwargs):
     url = '{addon_name}?{query_string}'.format(addon_name = sys.argv[0], query_string = urllib.urlencode(query_string))
     liz = xbmcgui.ListItem(name)
     info_labels = {"Title": name}
-    for k, v in kwargs.iteritems():
-        if not v:
-            continue
-        if k == 'info_labels':
-            info_labels = dict(info_labels.items() + v.items())
-        if k == 'list_properties':
-            for list_property_key, list_property_value in v.iteritems():
-                liz.setProperty(list_property_key, list_property_value)
-        if k == 'art':
-            liz.setArt(v)
-            url = '{url}&{art_params}'.format(url = url, art_params = urllib.urlencode(v))
+    if 'info_labels' in kwargs:
+        inf_lbl = kwargs['info_labels']
+        info_labels = dict(info_labels.items() + inf_lbl.items())
+    if 'list_properties' in kwargs:
+        list_properties = kwargs['list_properties']
+        for list_property_key, list_property_value in list_properties.iteritems():
+            liz.setProperty(list_property_key, list_property_value)
+    if 'art' in kwargs:
+        art = kwargs['art']
+        liz.setArt(art)
+        url = '{url}&{art_params}'.format(url = url, art_params = urllib.urlencode(art))
+    if 'page' in kwargs:
+        url = '{url}&page={page}'.format(url = url, page = kwargs['page'])
     liz.setInfo(type = "Video", infoLabels = info_labels)
     return xbmcplugin.addDirectoryItem(handle = this_plugin, url = url, listitem = liz, isFolder = is_folder)
 
@@ -112,30 +118,26 @@ def get_genres():
 
 def get_shows():
     itemId = json.loads(id)
-    for i in range(9):
-        params = itemId.copy()
-        params['sorting'] = 'desc'
-        params['offset'] = i
-        url = build_url('/getList', params = params)
-        data = get_json_response(url)
-        if data:
-            for d in data:
-                add_dir(d['textHead'], d['ID'], mode_episode, art = {'thumb': d['thumbnail']})
-        else:
-            break
+    params = itemId.copy()
+    params['sorting'] = 'desc'
+    params['offset'] = page
+    url = build_url('/getList', params = params)
+    data = get_json_response(url)
+    if data:
+        for d in data:
+            add_dir(d['textHead'], d['ID'], mode_episode, art = {'thumb': d['thumbnail'].encode('utf8')})
+        add_dir('Next >>', id, mode_show, page = page + 1)
     xbmcplugin.endOfDirectory(this_plugin)
 
 def get_episodes():
-    for i in range(9):
-        params = {'showID': id, 'offset': i, 'sorting': 'desc'}
-        url = build_url('/getEpisodes', params = params)
-        data = get_json_response(url)
-        if data:
-            for d in data:
-                art = {'thumb': d['Thumbnail'], 'fanart': d['Large']}
-                add_dir(d['title'], d['id'], mode_play, art = art)
-        else:
-            break
+    params = {'showID': id, 'offset': page, 'sorting': 'desc'}
+    url = build_url('/getEpisodes', params = params)
+    data = get_json_response(url)
+    if data:
+        for d in data:
+            art = {'thumb': d['Thumbnail'].encode('utf8'), 'fanart': d['Large'].encode('utf8')}
+            add_dir(d['title'], d['id'], mode_play, is_folder = False, art = art, list_properties = {'isPlayable': 'true'})
+        add_dir('Next >>', id, mode_episode, page = page + 1)
     xbmcplugin.endOfDirectory(this_plugin)
 
 def get_show_player():
@@ -151,8 +153,7 @@ def play_episode():
     liz = xbmcgui.ListItem(name)
     liz.setInfo(type="Video", infoLabels={"Title": name})
     liz.setPath(video_url)
-    MODE_PLAY_LIVE = mode_play
-    if mode == MODE_PLAY_LIVE:
+    if mode == mode_play_live:
         xbmc.Player().play(item = video_url, listitem = liz)
     else:
         return xbmcplugin.setResolvedUrl(this_plugin, True, liz)
@@ -191,11 +192,38 @@ def get_access_token(allow_cached = True):
 def try_get_param(params, name, default_value = None):
     return params[name][0] if name in params else default_value
 
+def is_x_forwarded_for_ip_valid():
+    x_forwarded_for_ip = xbmcaddon.Addon().getSetting('xForwardedForIp').strip()
+    if x_forwarded_for_ip == '0.0.0.0' or x_forwarded_for_ip == '':
+        return False
+    return True
+
+def auto_generate_ip():
+    ip_range_list = [
+        (1848401920, 1848406015),
+        (1884172288, 1884176383),
+        (1931427840, 1931431935),
+        (2000617472, 2000621567),
+        (2070704128, 2070708223),
+    ]
+
+    start_ip_number, end_ip_number = ip_range_list[randint(0, len(ip_range_list) - 1)]
+    ip_number = randint(start_ip_number, end_ip_number)
+    w = (ip_number / 16777216) % 256
+    x = (ip_number / 65536) % 256
+    y = (ip_number / 256) % 256
+    z = (ip_number) % 256
+    if z == 0: z = 1
+    if z == 255: z = 254
+    ip_address = '%s.%s.%s.%s' % (w, x, y, z)
+    xbmcaddon.Addon().setSetting('xForwardedForIp', ip_address)
+
 mode = mode_page
 params = urlparse.parse_qs(sys.argv[2].replace('?',''))
 name = try_get_param(params, 'name')
 mode = int(try_get_param(params, 'mode', mode))
 thumb = try_get_param(params, 'thumb', '')
+page = int(try_get_param(params, 'page', 0))
 id = try_get_param(params, 'id')
 
 if mode == mode_page or not id or len(id) == 0:
